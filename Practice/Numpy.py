d416bac0610463155e54529b4036c3f21a2c6669
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2026-09-08 10:53:44 +0530

    changed directory

---
+++ Practice/Numpy.py
@@ -0,0 +1,4 @@
+#Numpy stands for Numerical python and is the core library for numeric and scientific computing.
+#It consists of multidimensional array objects and a collection of routines for processing those arrays.
+
+#Single Dimensional Array:
