1f4d9973e0d02c192a187fda3010ca46aa3f54e0
chaitanyanirmale <chaitanyanirmale04@gmail.com>
2026-09-12 21:36:59 +0530

    practice examples on exception handling

--- Exception Handling/practice.py
+++ Exception Handling/practice.py
@@ -1,37 +1,37 @@
 #Division Calculator
-# try:
-#   num1 = int(input("Enter First Number:" ))
-#   num2 = int(input("Enter Second Number:" ))
-#   result = num1 / num2
-#   print(result)
+try:
+  num1 = int(input("Enter First Number:" ))
+  num2 = int(input("Enter Second Number:" ))
+  result = num1 / num2
+  print(result)
 
-# except ValueError: 
-#   print('Enter valid numbers')
+except ValueError: 
+  print('Enter valid numbers')
 
-# except ZeroDivisionError:
-#   print('Number can not be zero')
+except ZeroDivisionError:
+  print('Number can not be zero')
 
-
+#------------------------------------------
 #Age Validation
-# age = int(input('Enter the Age: '))
-# if age < 0:
-#   raise ValueError('Age can not be negative')
-# if age < 18: 
-#   print('You are a minor')
-# if age >= 18:
-#   print('You are a adult')
+age = int(input('Enter the Age: '))
+if age < 0:
+  raise ValueError('Age can not be negative')
+if age < 18: 
+  print('You are a minor')
+if age >= 18:
+  print('You are a adult')
 
-
+#----------------------------------------
 #Employee Salary
-# def calculate_salary(salary):
-#   if salary < 0:
-#     raise ValueError('Salary can not be negative')
-#   print(salary)
+def calculate_salary(salary):
+  if salary < 0:
+    raise ValueError('Salary can not be negative')
+  print(salary)
 
-# salary = int(input('Enter salary: '))
-# calculate_salary(salary)   
+salary = int(input('Enter salary: '))
+calculate_salary(salary)   
 
-
+#------------------------------------------
 # List Search
 numbers = [10, 20, 30, 40, 50]
 idx = int(input("Enter index: "))
@@ -39,4 +39,33 @@ idx = int(input("Enter index: "))
 if idx > len(numbers) or idx < 0:
   raise IndexError('Invalid Index value')
 
-print(numbers[idx])    
+print(numbers[idx]) 
+#-----------------------------------------
+employee = {
+  "name": "Chaitanya",
+  "role": "Developer",
+  "salary": 40000
+}
+
+key = input('Enter key: ')
+
+if key not in employee:
+  raise KeyError("Key doesn't exist")
+
+print(employee[key])
+
+#--------------------------------------
+#Login system
+username = 'admin'
+password = '1234'
+
+user_name = input('Enter username: ')
+user_password = input('Enter password: ')
+
+try: 
+  if username == user_name and password == user_password:
+    print('Login Successfully!')
+  else:
+    raise ValueError('Invalid username or password')
+except ValueError as e:
+  print('Login Failed: ', e)
